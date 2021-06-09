3c1a61123209e0c6537f406ca59c580e05616576
cerasus1004 <cerasus1004@gmail.com>
2021-06-09 11:20:37 +0300

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
-//Открытие меню
+'use strict'
 
+//Открытие меню
 const pageNav = document.querySelector(".header__nav");
 pageNav.classList.remove("header__nav--nojs");
 pageNav.classList.add("header__nav--closed");
@@ -16,30 +17,47 @@ navToggle.addEventListener("click", function () {
 });
 
 const btns = document.querySelectorAll(`.modal-js`);
-  const modal = document.querySelector(`.pay-modal`);
-  const btnClose = document.querySelector(`.pay-modal__btn-close`);
-  const overlay = document.querySelector(`.pay-modal-overlay`);
-  const closeModalBtn = () => {
+const modal = document.querySelector(`.pay-modal`);
+const btnClose = document.querySelector(`.pay-modal__btn-close`);
+const overlay = document.querySelector(`.pay-modal-overlay`);
+const closeModalBtn = () => {
+  modal.classList.remove(`pay-modal--active`);
+  overlay.classList.remove(`pay-modal-overlay--active`);
+  btnClose.removeEventListener(`click`, closeModalBtn);
+  overlay.removeEventListener(`click`, closeModalBtn);
+};
+const closeModalEsc = (evt) => {
+  if (evt.key === `Escape`) {
+    evt.preventDefault();
     modal.classList.remove(`pay-modal--active`);
     overlay.classList.remove(`pay-modal-overlay--active`);
-    btnClose.removeEventListener(`click`, closeModalBtn);
-    overlay.removeEventListener(`click`, closeModalBtn);
-  };
-  const closeModalEsc = (evt) => {
-    if (evt.key === `Escape`) {
-      evt.preventDefault();
-      modal.classList.remove(`pay-modal--active`);
-      overlay.classList.remove(`pay-modal-overlay--active`);
-    }
-    window.removeEventListener(`keydown`, closeModalEsc);
-  };
-  for (let i = 0; i < btns.length; i++) {
-    btns[i].addEventListener(`click`, (evt) => {
-      evt.preventDefault();
-      modal.classList.add(`pay-modal--active`);
-      overlay.classList.add(`pay-modal-overlay--active`);
-      btnClose.addEventListener(`click`, closeModalBtn);
-      window.addEventListener(`keydown`, closeModalEsc);
-      overlay.addEventListener(`click`, closeModalBtn);
-    });
   }
+  window.removeEventListener(`keydown`, closeModalEsc);
+};
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+    modal.classList.add(`pay-modal--active`);
+    overlay.classList.add(`pay-modal-overlay--active`);
+    btnClose.addEventListener(`click`, closeModalBtn);
+    window.addEventListener(`keydown`, closeModalEsc);
+    overlay.addEventListener(`click`, closeModalBtn);
+  });
+}
+
+const tabs = document.querySelectorAll(`.tabs__item`); 
+const item = document.querySelectorAll(`.list__item`); 
+
+tabs.forEach(tab => tab.addEventListener('click', tabClick));
+
+function tabClick(evt) {
+  const tabId = evt.target.dataset.id;
+
+  tabs.forEach((tab, i) => {
+    tab.classList.remove(`tabs__item--active`);
+    item[i].classList.remove(`active`);
+  })
+
+  tabs[tabId - 1].classList.add(`tabs__item--active`);
+  item[tabId - 1].classList.add(`active`);
+}
